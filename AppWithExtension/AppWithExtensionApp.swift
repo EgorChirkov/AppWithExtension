30006d9e93c98ef06148c109b6a70c3593bc253e
Egor <egorchirkov03@gmail.com>
2022-11-24 12:19:28 +0300

    Initial Commit

---
+++ AppWithExtension/AppWithExtensionApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppWithExtensionApp.swift
+//  AppWithExtension
+//
+//  Created by Егор Чирков on 24.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct AppWithExtensionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
